fce350d8f4ce04056c2113cfb0467f180d6738c0
Thomas Porter <thomasmporterii@gmail.com>
2017-11-21 13:17:11 -0700

    Possible Cleanup from Linter

--- src/Introduction.fsx
+++ src/Introduction.fsx
@@ -6,16 +6,19 @@
 //---ASSIGN A VALUE---
 //I Do: add 3 and 4 and assign it to a value called 'lucky'
 let lucky = 3 + 4
+printfn "lucky is %i" lucky
 //You Do: add 8 and 7 and assign it to a value called 'unlucky'
 
 //---CREATE A FUNCTION---
 //I Do: create a function named 'add' that takes two ints and adds them together
 let add x y = x + y
+add 3 4
 //You Do: create a function named 'multiply' that takes two ints and adds them together
 
 //---CREATE A SEQUENCE---
 //I Do: create a Sequence named 'firstTen' that contains the numbers 1 to 10
 let firstTen = [1 .. 10]
+printfn "The first 10 numbers are %A" firstTen
 //You Do: create a Sequence named 'firstHundred' that contains the numbers 0 to 100
 
 //---USE A HIGH ORDER FUNCTION WITH A NAMED FUNCTION---
@@ -25,7 +28,6 @@ let firstTen = [1 .. 10]
 let addOne x = x + 1
 [1 .. 10]
 |> Seq.map(addOne)
-
 //You Do: create a function that takes in an int and multiplies it by 10
 //then create a Sequence that contains the numbers 0 to 100
 //then use the map high-order function to apply the named function to the sequence
@@ -60,14 +62,16 @@ type Gender =
 //---CREATING A RECORD TYPE---
 //I Do: create a record type of a person that has name(string), age(float), gender(Gender)
 //then create an instance of that type named 'Jamie' aged 42.5, and is a male
-type Person = {Name:string;Age:float;Gender:Gender}
-let jamie = {Name="Jamie";Age=42.5;Gender=Male}
+type Person = {Name:string; Age:float; Gender:Gender}
+let jamie = {Name="Jamie"; Age=42.5; Gender=Male}
+printfn "Jamie's information %A" jamie
 //You Do:create a record type of a Pet Toy that has a name(string), rating(float), and Pet(Pet)
 //then create an instance of that type named 'Busy Bone', 4.8 rating, and is for a Dog
 
 //---CREATE A TUPLE---
-//I Do: create a tuple called of 'Jamie' and '42'
-let jamie = "Jamie",42
+//I Do: create a tuple called jamieTuple of 'Jamie' and '42'
+let jamieTuple = "Jamie", 42
+printfn "Jamie's information %A" jamieTuple
 //You Do:create a tuple called 'YourName' and '24'
 
 
